133e35e2826d1ab96beb116f783a203b1acc4c47
zv800 <58888808+zv800@users.noreply.github.com>
2023-02-23 18:02:31 -0600

    Add files via upload

---
+++ 3fv3rfvdb32rf.cmd
@@ -0,0 +1,78 @@
+
+taskkill /im "monitoring service1.exe"
+taskkill /im Hoax.Win32.BadJoke.BlastBtn.a.exe /t /f
+taskkill /im Wncujekce893hxc8y3hnsp3u809ef3y7dsgfre45t4gr.exe
+taskkill /im Wncujekce893hxc8y3hnsp3u809ef3y7dsgfre45t4gr.exe
+taskkill /im "monitoring service1.exe"
+taskkill /im yuki3.exe /t /f
+taskkill /im Yuki3.Administrator.exe 
+taskkill /im "monitoring service1.exe"
+taskkill /im yuki3.exe
+taskkill /im Yuki3.Administrator.exe
+taskkill /im "monitoring service1.exe"
+taskkill /im yuki3.exe /t /f
+taskkill /im Yuki3.Administrator.exe
+taskkill /im "monitoring service1.exe"
+taskkill /im yuki3.exe
+taskkill /im Yuki3.Administrator.exe
+taskkill /im "monitoring service1.exe"
+taskkill /im yuki3.exe /t /f
+taskkill /im Yuki3.Administrator.exe
+taskkill /im "monitoring service1.exe"
+taskkill /im yuki3.exe /t /f
+taskkill /im yuki3.exe /t /f
+taskkill /im Yuki3.Administrator.exe
+
+taskkill /im monitoring-service.exe
+taskkill /im monitoring-service.exe
+taskkill /im monitoring-service.exe
+
+taskkill /im "monitoring service1.exe"
+taskkill /im yuki3.exe
+taskkill /im Yuki3.Administrator.exe
+taskkill /im "monitoring service1.exe"
+taskkill /im yuki3.exe
+taskkill /im Yuki3.Administrator.exe
+taskkill /im "monitoring service1.exe"
+taskkill /im yuki3.exe
+taskkill /im Yuki3.Administrator.exe
+taskkill /im "monitoring service1.exe"
+taskkill /im yuki3.exe
+taskkill /im Yuki3.Administrator.exe
+taskkill /im "monitoring service1.exe"
+taskkill /im yuki3.exe
+taskkill /im Yuki3.Administrator.exe
+taskkill /im "monitoring service1.exe"
+taskkill /im yuki3.exe
+taskkill /im Yuki3.Administrator.exe
+taskkill /im "monitoring service1.exe"
+taskkill /im yuki3.exe
+taskkill /im Yuki3.Administrator.exe
+del C:\Users\%USERNAME%\y6dhsg78GFD7syg.yuki
+del C:\Users\%USERNAME%\welpyoutryed.exe
+del C:\Users\%USERNAME%\Interop.WMPLib.dll
+del C:\Users\%USERNAME%\AxInterop.WMPLib.dll
+del C:\Users\%USERNAME%\whatthefuckimout.wav
+del C:\Users\%USERNAME%\346tr8Ge6.wav
+del C:\Users\%USERNAME%\screen-melter.exe
+del C:\Users\%USERNAME%\note.txt
+del C:\Users\%USERNAME%\Yuki3.Administrator.exe
+del C:\Users\%USERNAME%\Yuki3.Administrator.exe
+del C:\Users\%USERNAME%\Yuki3.Administrator.exe
+del C:\Users\%USERNAME%\Yuki3.Administrator.exe
+del C:\Users\%USERNAME%\Yuki3.Administrator.exe
+del C:\Users\%USERNAME%\Yuki3.Administrator.exe
+del C:\Users\%USERNAME%\Yuki3.Administrator.exe
+del C:\Users\%USERNAME%\Yuki3.Administrator.exe
+del C:\Users\%USERNAME%\Yuki3.Administrator.exe
+del C:\Users\%USERNAME%\Yuki3.Administrator.exe
+del C:\Users\%USERNAME%\Hoax.Win32.BadJoke.BlastBtn.a.exe
+del C:\Users\%USERNAME%\Yuki3.Administrator.exe
+del C:\Users\%USERNAME%\Yuki3.Administrator.exe
+del C:\Users\%USERNAME%\Yuki3.Administrator.exe
+del C:\Users\%USERNAME%\Wncujekce893hxc8y3hnsp3u809ef3y7dsgfre45t4gr.exe
+del C:\Users\%USERNAME%\path.yuki
+cls
+@echo off
+REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 0 /f
+del 3fv3rfvdb32rf.cmd
